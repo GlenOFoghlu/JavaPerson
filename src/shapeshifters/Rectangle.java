77ddc1d853790dc8806dd03f95b4352e83b91f46
Nemo <Niamh.foley2@gmail.com>
2019-08-04 00:21:00 +0100

    'drawing'

--- src/shapeshifters/Rectangle.java
+++ src/shapeshifters/Rectangle.java
@@ -27,11 +27,26 @@ public class Rectangle extends Shape {
 
 		   @Override
 		   public void drawMe() {
-			   for (int i = 1; i <= length; i++) {
-			         for (int j = 0; j < width; j++) {
-			            System.out.print("-");			           
-			         }
-			        System.out.println("+");
-			      }			   
+
+			    for(int i=1; i<=length; i++)
+			    { 
+			        for(int j=1; j<=width; j++)
+			        {
+			            if(i==1 || i==length || j==1 || j==width)
+			            {
+//			               if (i==1 ||)
+//			               {
+//			            	   System.out.print("|");  
+//			               }
+			            	System.out.print("-");
+			            }
+			            else
+			            {
+			            	System.out.print(" ");
+			            }
+			        }
+
+			        System.out.println();
+			    }
 		   }
 }
